fd6156efe294892e805929d42096e5965ba36d91
Bahtiyar KARAKOC <bahtiyar093@gmail.com>
2022-12-21 04:13:03 +0300

    Project Setup.

--- MovieScheduler/src/App.tsx
+++ MovieScheduler/src/App.tsx
@@ -1,7 +1,12 @@
-import Hello from '~/components/Hello';
+import React from "react";
+import Selam from "./selam";
 
 function App() {
-  return <Hello />;
+  return (
+    <div>
+      <Selam />
+    </div>
+  );
 }
 
 export default App;
---
+++ MovieScheduler/src/selam.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Selam() {
+  return (
+    <div>
+      <h1 className="text-4xl">selam</h1>
+    </div>
+  );
+}
+
+export default Selam;
